0b5f32f28be5c4700f0720bde08ce6ff9a6a972a
wiwibug <abdelatif.wiam@gmail.com>
2026-03-04 21:35:47 +0100

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import dash
-from app.app import app as application  # Renommer pour éviter les conflits
-
-server = application.server  # 👈 LIGNE OBLIGATOIRE pour Plotly Cloud
-
-if __name__ == "__main__":
-    application.run_server(debug=True)
