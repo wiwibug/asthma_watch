8574f921b031c92eaf2f8e03cfdf47f69d361c84
wiwibug <abdelatif.wiam@gmail.com>
2026-03-04 21:43:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-import dash
-from app.app import app as application  
+import sys
+import os
+from pathlib import Path
 
-server = application.server 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from app.dashboard_app import app
+
+server = app.server
 
 if __name__ == "__main__":
-    application.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0", port=8050)
